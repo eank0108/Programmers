f65fd8e19717f40619e805011261aa2217098c68
eank0108 <eank0108@naver.com>
2022-05-14 23:29:46 +0900

    정답

--- Q31.java
+++ Q31.java
@@ -1,2 +1,15 @@
-public class Q31 {
+class Q31 {
+    public int solution(int[][] sizes) {
+        int answer = 0;
+        int[] wallet = {0,0};
+        for(int i = 0; i < sizes.length; i ++){
+            int[] size = sizes[i];
+            int maxWH = Math.max(size[0],size[1]);
+            int minWH = Math.min(size[0],size[1]);
+            wallet[0] = Math.max(wallet[0],maxWH);
+            wallet[1] = Math.max(wallet[1],minWH);
+
+        }
+        return wallet[0] * wallet[1];
+    }
 }
